e1d2ad32a475cf9142a0e36ef92d7be78a7e5f67
Veera Kishon Kumar Mucherla <veerakishon.mucherla@gmail.com>
2018-06-11 00:29:51 -0400

    Converted project type to maven configurable and added BL to call a procedure present in the database

--- src/com/cr7/jdbcoperations/SatementDBImp.java
+++ src/com/cr7/jdbcoperations/SatementDBImp.java
@@ -1,9 +1,13 @@
 package com.cr7.jdbcoperations;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
+import java.sql.Types;
 
 /**
  * @author GameS
@@ -15,14 +19,25 @@ public class SatementDBImp {
 	 */
 	public void getDeptDetails(int deptNo) {
 		Statement statement = null;
+		CallableStatement statement1 = null;
 		ResultSet res = null;
 		Connection conn = null;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "System", "Mucherlas1");
 			statement = conn.createStatement();
-			String q1 = "select * from dept";
+			String q1 = "select * from dept where deptno = " + deptNo;
 			res = statement.executeQuery(q1);
+			ResultSetMetaData rmd = res.getMetaData();
+			System.out.println(rmd.getColumnName(1));
+			DatabaseMetaData dmd = conn.getMetaData();
+			System.out.println(dmd.getDatabaseProductName());
+			// made call to the sql procedure present in teh database
+			statement1 = conn.prepareCall("{call getLoc(?,?)}");
+			statement1.setInt(1, 10);
+			statement1.registerOutParameter(2, Types.CHAR);
+			statement1.execute();
+			System.out.println(statement1.getString(2));
 			while (res.next()) {
 				System.out.println(res.getInt(1));
 				System.out.println(res.getString(2));
